5711dc08b9fb1d480e8549bee5da303d52ed6dfb
Fernando Barbosa <77360898+FernandoBarbosaaa@users.noreply.github.com>
2026-03-18 16:50:30 -0300

    Implement database creation and inspection
    Added database initialization and inspection logic.

--- comunidadeimpressionadora/__init__.py
+++ comunidadeimpressionadora/__init__.py
@@ -4,6 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 import os
+import sqlalchemy
+from comunidadeimpressionadora.models import Usuario, Post
 
 # Pasta do pacote (onde estão static/ e templates/)
 pasta_pacote = os.path.dirname(os.path.abspath(__file__))
@@ -25,5 +27,15 @@ login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message = 'Faça login para acessar esta página.'
 login_manager.login_message_category = 'alert-primary'
+
+engine = sqlalchemy.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+inspector = sqlalchemy.inspect(engine)
+if not inspector.has_table("usuario"):
+    with app.app_context():
+        database.drop_all()
+        database.create_all()
+        print("Base de Dados criado")
+else:
+    print("Base de Dados já existente")
 
 from comunidadeimpressionadora import routes
