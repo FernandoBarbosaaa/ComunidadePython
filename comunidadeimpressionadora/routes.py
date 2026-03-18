7fb6619cced7c35e041b4353f5d41d450decf613
Fernando Barbosa <77360898+FernandoBarbosaaa@users.noreply.github.com>
2026-03-18 17:20:36 -0300

    Refactor routes.py with consistent formatting

--- comunidadeimpressionadora/routes.py
+++ comunidadeimpressionadora/routes.py
@@ -52,7 +52,7 @@ def login():
 
     if form_criarconta.validate_on_submit() and 'botao_submit_criarconta' in request.form:
         #criptografar a senha
-        senha_cript = bcrypt.generate_password_hash(form_criarconta.senha.data)
+        senha_cript = bcrypt.generate_password_hash(form_criarconta.senha.data).decode('utf-8')
         #criar usuario
         usuario = Usuario(username=form_criarconta.username.data, email=form_criarconta.email.data, senha=senha_cript)
         #adicionar usuario ao banco de dados
